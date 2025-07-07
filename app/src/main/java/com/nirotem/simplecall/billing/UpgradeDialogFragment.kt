ff26ae51d1fc62891613780adee5497e0d62abc0
Nir <nir@to-breath-again.com>
2025-07-07 06:43:59 +0300

    After adding Subscription page. 7/7/2025

--- app/src/main/java/com/nirotem/simplecall/billing/UpgradeDialogFragment.kt
+++ app/src/main/java/com/nirotem/simplecall/billing/UpgradeDialogFragment.kt
@@ -1,5 +1,6 @@
 package com.nirotem.simplecall.billing
 
+import android.animation.AnimatorInflater
 import android.app.Dialog
 import android.content.DialogInterface
 import android.content.res.Resources
@@ -7,8 +8,12 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Typeface
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.*
 import android.widget.*
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.view.ViewCompat
 import androidx.fragment.app.DialogFragment
 import com.nirotem.simplecall.R
 
@@ -41,28 +46,27 @@ class UpgradeDialogFragment(
             (resources.displayMetrics.widthPixels * 0.96).toInt(),
             (resources.displayMetrics.heightPixels * 0.88).toInt()
         )
-        dialog?.window?.setBackgroundDrawableResource(R.drawable.dialog_dark_rounded_background)
-        dialog?.window?.setDimAmount(0.6f)
+       // dialog?.window?.setBackgroundDrawableResource(R.drawable.dialog_dark_rounded_background)
+        //dialog?.window?.setDimAmount(0.6f)
     }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val btnClose = view.findViewById<View>(R.id.upgrade_dialog_close_button)
+       // val btnClose = view.findViewById<View>(R.id.upgrade_dialog_close_button)
         val btnStandard = view.findViewById<Button>(R.id.btnBasic)
         val btnPremium = view.findViewById<Button>(R.id.btnPremium)
-        val title = view.findViewById<TextView>(R.id.upgradeTitle)
         val basicPriceMonthly = view.findViewById<TextView>(R.id.basicPriceMonthly)
         val basicPriceYearly = view.findViewById<TextView>(R.id.basicPriceYearly)
         val premiumPriceMonthly = view.findViewById<TextView>(R.id.premiumPriceMonthly)
         val premiumPriceYearly = view.findViewById<TextView>(R.id.premiumPriceYearly)
-        val premiumOriginalPrice = view.findViewById<TextView>(R.id.premiumOriginalPrice)
+/*        val premiumOriginalPrice = view.findViewById<TextView>(R.id.premiumOriginalPrice)
         premiumOriginalPrice.apply {
             paintFlags = paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
         }
         val basicOriginalPrice = view.findViewById<TextView>(R.id.basicOriginalPrice)
         basicOriginalPrice.apply {
             paintFlags = paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-        }
+        }*/
 
 
         val basicFeaturesContainer = view.findViewById<LinearLayout>(R.id.basicFeaturesContainer)
@@ -107,7 +111,7 @@ class UpgradeDialogFragment(
             "תקופת הניסיון הסתיימה – שדרג כדי לפתוח את כל הפיצ'רים"
         }*/
 
-        btnClose.setOnClickListener { dismiss() }
+       // btnClose.setOnClickListener { dismiss() }
 
         btnStandard.setOnClickListener {
             billingManager.launchPurchaseFlow(requireActivity(), "standard_subscription_id")
@@ -120,7 +124,7 @@ class UpgradeDialogFragment(
         val features = listOf(
             FeatureRow(view.context.getString(R.string.subscription_plan_feature_name_call_management), true, true),
             FeatureRow(view.context.getString(R.string.subscription_plan_feature_name_big_buttons_icons), true, true),
-            FeatureRow(view.context.getString(R.string.subscription_plan_feature_name_auto_answer), true, true),
+            FeatureRow(view.context.getString(R.string.subscription_plan_feature_name_click_to_answer), true, true),
             FeatureRow(view.context.getString(R.string.subscription_plan_feature_name_gold_number), true, true),
             FeatureRow(view.context.getString(R.string.subscription_plan_feature_name_quick_call), true, true),
             FeatureRow(view.context.getString(R.string.subscription_plan_feature_name_lock_screen), false, true),
@@ -159,6 +163,21 @@ class UpgradeDialogFragment(
         features.filter { !it.isStandard && it.isPremium }.forEach { feature ->
             premiumFeaturesContainer?.addView(createFeatureTextView(feature.title))
         }
+
+        val crownImage = view.findViewById<ImageView>(R.id.upgrade_plan_crown_image)
+        if (crownImage.layoutDirection == View.LAYOUT_DIRECTION_RTL) {
+            crownImage.scaleX = 1f
+        } else {
+            crownImage.scaleX = -1f // לוודא שבמצב LTR היא בכיוון הנכון
+        }
+        val shakeAnimator = AnimatorInflater.loadAnimator(requireContext(), R.animator.shake)
+        shakeAnimator.setTarget(crownImage)
+        shakeAnimator.start()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            shakeAnimator.cancel() // או shakeAnimator.end()
+        }, 3000)
+
     }
 
     override fun onDismiss(dialog: DialogInterface) {
@@ -169,19 +188,26 @@ class UpgradeDialogFragment(
     data class FeatureRow(val title: String, val isStandard: Boolean, val isPremium: Boolean)
 
     private fun createFeatureTextView(title: String, isHeader: Boolean = false): TextView {
-        return TextView(requireContext()).apply {
-            text = if (isHeader) title else "• $title"
+        val textView = TextView(requireContext()).apply {
+            text = title
             setTextColor(Color.WHITE)
-            textSize = if (isHeader) 13f else 14f
-            setTypeface(null, if (isHeader) Typeface.BOLD else Typeface.NORMAL)
+            textSize = 14f
+            gravity = Gravity.CENTER // מרכז את הטקסט בתוך עצמו
             layoutParams = LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.WRAP_CONTENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT
             ).apply {
-                topMargin = if (isHeader) 8.dpToPx() else 4.dpToPx()
+                gravity = Gravity.CENTER_HORIZONTAL // מרכז את ה־TextView בתוך ה־LinearLayout
+                topMargin = if (isHeader) 4.dpToPx() else 1.dpToPx()
                 bottomMargin = if (isHeader) 4.dpToPx() else 0
             }
         }
+
+        // הגדרת הפונט מתוך res/font/open_sans.xml
+        val typeface = ResourcesCompat.getFont(requireContext(), R.font.open_sans)
+        textView.setTypeface(typeface, if (isHeader) Typeface.BOLD else Typeface.NORMAL)
+
+        return textView
     }
 
 
